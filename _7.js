e7df90b260d6b9ca53dfc7033ce5816462fc5a72
al2bovek <aleksandr.dubovek@gmail.com>
2025-12-01 17:02:06 +0200

    commit -7.js t_ add select

--- _7.js
+++ _7.js
@@ -1,19 +1,20 @@
 // ## 7 DOM: Build a Temperature Converter  **3 points**
 const result = document.querySelector("#result")
 const value = document.querySelector('#value');
-// const unit = document.querySelector("#unit");
+const unit = document.querySelector("#unit");
 const celsius = document.querySelector('#celsius');
 const fahrenheit = document.querySelector('#fahrenheit');
 const btn = document.querySelector("#convert");
 value.addEventListener("focus", () =>  value.value = '');
-
-const formula = (temp, unit = celsius.value) => {
-    if(!unit) unit = fahrenheit.value
+let select = '';
+unit.addEventListener('click', (e) => select = e.target.value);
+const formula = (temp, unit = select) => {
+    if(!unit) unit = celsius.value;
     let result = '';
   if (unit === "celsius") {
-    result = `${(temp * 9/5) + 32}° Celsius`;
+    result = `${((temp * 9/5) + 32).toFixed(2)}° Celsius`;
   } else {
-    result = `${(temp - 32) * 5/9}° Fahrenheit`;
+    result = `${((temp - 32) * 5/9).toFixed(2)}° Fahrenheit`;
   }
   return result;
 }
